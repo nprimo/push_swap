8e0ac00deb76ba2590e0e080453df9c3e996da48
nprimo <primo.niccolo@gmail.com>
2021-12-16 11:03:00 +0000

    ft_swap.c - first commit
    added prototype in push_swap.h
    added quick test in test/main.c

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 10:40:21 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/16 11:02:12 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "libft/libft.h"
 
 t_list	*init_stack(int ac, char **av);
+void	ft_swap(t_list **stack);
 
 #endif
--- src/ft_swap.c
+++ src/ft_swap.c
@@ -1,40 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/16 10:23:08 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:02:34 by nprimo           ###   ########.fr       */
+/*   Created: 2021/12/16 10:54:11 by nprimo            #+#    #+#             */
+/*   Updated: 2021/12/16 11:01:43 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-#include <stdio.h>
-
-void	print_stack(t_list *head)
+void	ft_swap(t_list **stack)
 {
-	while (head)
-	{
-		printf("%s\n", head->content);
-		head = head->next;
-	}
-}
+	t_list	*curr_head;
+	t_list	*new_head;
 
-int	main(int ac, char **av)
-{
-	t_list	*stack_a;
-
-	stack_a = NULL;
-	if (ac > 1)
-	{
-		// Check init_stack
-		stack_a = init_stack(ac, av);
-		print_stack(stack_a); 
-		// Check swap
-		ft_swap(&stack_a);
-		print_stack(stack_a);
-	}
+	curr_head = *stack;
+	new_head = (*stack)->next;
+	curr_head->next = new_head->next;
+	new_head->next = curr_head;
+	*stack = new_head;
 }

dd576f618eb0976ec2b7aba00a471134335ead87
nprimo <primo.niccolo@gmail.com>
2021-12-16 11:32:57 +0000

    ft_push.c - first commit
    Added prototype to push_swap.h
    added tests in test/main.c

--- src/ft_push.c
+++ src/ft_push.c
@@ -1,22 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_push.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:26:08 by nprimo           ###   ########.fr       */
+/*   Created: 2021/12/16 11:21:00 by nprimo            #+#    #+#             */
+/*   Updated: 2021/12/16 11:29:51 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft/libft.h"
+void	ft_push(t_list **getter, t_list **giver)
+{
+	t_list	*curr_head;
+	t_list	*new_head;
 
-t_list	*init_stack(int ac, char **av);
-void	ft_swap(t_list **stack);
-void	ft_push(t_list **getter, t_list **giver);
-
-#endif
+	if (!(*giver))
+		return ;
+	curr_head = *giver;
+	new_head = (*giver)->next;
+	curr_head->next = NULL;
+	ft_lstadd_front(getter, curr_head);
+	*giver = new_head;
+}
--- test/main.c
+++ test/main.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:23:08 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:02:34 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/16 11:31:51 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,34 @@ void	print_stack(t_list *head)
 		printf("%s\n", head->content);
 		head = head->next;
 	}
+	printf("---\n");
 }
 
 int	main(int ac, char **av)
 {
 	t_list	*stack_a;
+	t_list	*stack_b;
 
 	stack_a = NULL;
+	stack_b = NULL;
 	if (ac > 1)
 	{
 		// Check init_stack
+		printf("--Check init_stack--\n");
 		stack_a = init_stack(ac, av);
 		print_stack(stack_a); 
 		// Check swap
+		printf("--Check ft_swap--\n");
 		ft_swap(&stack_a);
 		print_stack(stack_a);
+		// Check push
+		printf("--Check ft_push (1)--\n");
+		ft_push(&stack_b, &stack_a);
+		print_stack(stack_a);
+		print_stack(stack_b);
+		printf("--Check ft_push (2)--\n");
+		ft_push(&stack_b, &stack_a);
+		print_stack(stack_a);
+		print_stack(stack_b);
 	}
 }

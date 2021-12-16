93b1b9a511f930bd8f5b333efa70f2b0b5ad79b7
nprimo <primo.niccolo@gmail.com>
2021-12-16 10:41:34 +0000

    init_stack.c - first commit
    updated push_swap.h with prototype as well

--- src/init_stack.c
+++ src/init_stack.c
@@ -1,20 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   init_stack.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 10:40:21 by nprimo           ###   ########.fr       */
+/*   Created: 2021/12/16 10:35:33 by nprimo            #+#    #+#             */
+/*   Updated: 2021/12/16 10:40:25 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft/libft.h"
+t_list	*init_stack(int ac, char **av)
+{
+	t_list	*stack;
+	int		pos;
 
-t_list	*init_stack(int ac, char **av);
-
-#endif
+	stack = NULL;
+	pos = 1;
+	while (pos < ac)
+	{
+		ft_lstadd_back(&stack, ft_lstnew(av[pos]));
+		pos++;
+	}
+	return (stack);
+}

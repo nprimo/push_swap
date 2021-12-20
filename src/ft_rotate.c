e50b7d6a3500bf665c321186d7f97151219fb8cb
nprimo <primo.niccolo@gmail.com>
2021-12-20 12:21:30 +0100

    ft_rotate.c - updated accoringly to new linked list struct

--- src/ft_rotate.c
+++ src/ft_rotate.c
@@ -6,23 +6,23 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 11:34:09 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:38:41 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 12:17:36 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_rotate(t_list **stack)
+void	ft_rotate(t_stack **stack)
 {
-	t_list	*curr_head;
-	t_list	*curr_last;
-	int		len;
+	t_stack	*curr_head;
+	t_stack	*curr_last;
+	int		size;
 
-	len = ft_lstsize(*stack);
-	if (len < 2 || !(*stack))
+	size = ft_stcsize(*stack);
+	if (size < 2 || !(*stack))
 		return ;
 	curr_head = *stack;
-	curr_last = ft_lstlast(*stack);
+	curr_last = ft_stclast(*stack);
 	*stack = (*stack)->next;
 	curr_head->next = NULL;
 	curr_last->next = curr_head;

d00b51e3836a1339ddf5d03f6814530b6120c6e5
nprimo <primo.niccolo@gmail.com>
2021-12-20 11:43:55 +0100

    ft_push.c - updated accordingly to new struct

--- src/ft_push.c
+++ src/ft_push.c
@@ -6,16 +6,16 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 11:21:00 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:29:51 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 11:43:47 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_push(t_list **getter, t_list **giver)
+void	ft_push(t_stack **getter, t_stack **giver)
 {
-	t_list	*curr_head;
-	t_list	*new_head;
+	t_stack	*curr_head;
+	t_stack	*new_head;
 
 	if (!(*giver))
 		return ;

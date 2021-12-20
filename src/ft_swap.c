a3fc3798f3944c1d5a674339c9ba7a6bf6ffb5f1
nprimo <primo.niccolo@gmail.com>
2021-12-20 15:55:38 +0100

    ft_swap.c - protected from empty stack

--- src/ft_swap.c
+++ src/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:54:11 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/20 11:42:54 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 15:55:09 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_swap(t_stack **stack)
 	t_stack	*curr_head;
 	t_stack	*new_head;
 
+	if (!(*stack))
+		return ;
 	curr_head = *stack;
 	new_head = (*stack)->next;
 	curr_head->next = new_head->next;

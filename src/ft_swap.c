d6853d648cedca1ce20238e173ddcf4d813793d0
nprimo <primo.niccolo@gmail.com>
2021-12-20 11:43:04 +0100

    ft_swap.c - updated accordingly to new struct

--- src/ft_swap.c
+++ src/ft_swap.c
@@ -6,16 +6,16 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:54:11 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 11:01:43 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 11:42:54 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_swap(t_list **stack)
+void	ft_swap(t_stack **stack)
 {
-	t_list	*curr_head;
-	t_list	*new_head;
+	t_stack	*curr_head;
+	t_stack	*new_head;
 
 	curr_head = *stack;
 	new_head = (*stack)->next;

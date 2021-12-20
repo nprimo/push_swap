7e51354a6e078b9cba6157228c21fd4895861d52
nprimo <primo.niccolo@gmail.com>
2021-12-20 09:49:28 +0100

    Added typedef t_stack
    a linked list with the following
    int	num
    t_stack	*next

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/16 12:04:57 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 09:47:50 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,12 @@
 # define PUSH_SWAP_H
 
 # include "libft/libft.h"
+
+typedef struct s_stack
+{
+	int		num;
+	t_stack	*next;
+}	t_stack;
 
 t_list	*init_stack(int ac, char **av);
 void	ft_swap(t_list **stack);

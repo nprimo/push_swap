c4f31f321b1d19f234090640ce074d4ca71e55bd
nprimo <primo.niccolo@gmail.com>
2021-12-20 18:04:38 +0100

    ft_swap.c - now protected
    from stack size less than 2 and empty size

--- src/ft_swap.c
+++ src/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:54:11 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/20 15:55:09 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/20 18:03:04 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	ft_swap(t_stack **stack)
 {
 	t_stack	*curr_head;
 	t_stack	*new_head;
+	int		size;
 
-	if (!(*stack))
+	size = ft_stcsize(*stack);
+	if (!(*stack) || size < 2)
 		return ;
 	curr_head = *stack;
 	new_head = (*stack)->next;

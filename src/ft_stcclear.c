396eb771c6267bafd9abc00da89adaa544155ffe
nprimo <primo.niccolo@gmail.com>
2021-12-23 12:01:48 +0100

    ft_stcclear - fixed bug
    now freeing memory in a proper way
    change order of actions

--- src/ft_stcclear.c
+++ src/ft_stcclear.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 19:41:56 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/20 19:46:43 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/23 12:01:05 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ void	ft_stcclear(t_stack **stack)
 	while (head)
 	{
 		tmp = head;
-		free(head);
 		head = tmp->next;
+		free(tmp);
 	}
+	*stack = NULL;
 }

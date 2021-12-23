e881fd73c23246b1f20d287ba935d54941088549
nprimo <primo.niccolo@gmail.com>
2021-12-23 15:06:53 +0100

    init_stack_test.c - first commit

--- tests/test.c
+++ tests/test.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:23:08 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/23 13:15:06 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/23 15:00:41 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,7 @@ int	main(int ac, char **av)
 	ft_stcadd_front_test();
 	ft_stcsize_test();
 	ft_stcclear_test();
+	init_stack_test();
 	stack_a = NULL;
 	stack_b = NULL;
 	if (ac > 1)
--- tests/test.h
+++ tests/test.h
@@ -6,18 +6,22 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 12:30:54 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/23 13:14:51 by nprimo           ###   ########.fr       */
+/*   Updated: 2021/12/23 15:01:31 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TEST_H
 # define TEST_H
 
+// test linked list basic func
 void	ft_stcnew_test(void);
 void	ft_stcadd_back_test(void);
 void	ft_stclast_test(void);
 void	ft_stcadd_front_test(void);
 void	ft_stcsize_test(void);
 void	ft_stcclear_test(void);
+
+// test stack basic func
+void	init_stack_test(void);
 
 #endif

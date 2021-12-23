f06844a0dee542b2b293af0ac1f0e33077f23610
nprimo <primo.niccolo@gmail.com>
2021-12-23 13:10:35 +0100

    ft_stcsize_test.c - first commit

--- tests/ft_stcsize_test.c
+++ tests/ft_stcsize_test.c
@@ -1,22 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.h                                             :+:      :+:    :+:   */
+/*   ft_stcsize_test.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/23 12:30:54 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/23 13:09:48 by nprimo           ###   ########.fr       */
+/*   Created: 2021/12/23 13:06:10 by nprimo            #+#    #+#             */
+/*   Updated: 2021/12/23 13:10:11 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef TEST_H
-# define TEST_H
+#include "push_swap.h"
 
-void	ft_stcnew_test(void);
-void	ft_stcadd_back_test(void);
-void	ft_stclast_test(void);
-void	ft_stcadd_front_test(void);
-void	ft_stcsize_test(void);
+void	ft_stcsize_test(void)
+{
+	t_stack *stc = ft_stcnew(1);
 
-#endif
+	ft_stcadd_front(&stc, ft_stcnew(2));
+	ft_stcadd_front(&stc, ft_stcnew(2));
+	ft_stcadd_front(&stc, ft_stcnew(2));
+	ft_stcadd_front(&stc, ft_stcnew(2));
+	ft_putstr_fd("--test ft_stcsize:\n", 1);
+	/*1*/
+	if (ft_stcsize(stc) != 5)
+		ft_putstr_fd("1.KO\t", 1);
+	else
+		ft_putstr_fd("1.Ok\t", 1);
+	ft_putstr_fd("\n", 1);
+}

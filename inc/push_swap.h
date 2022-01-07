51b8e6a7da77687de74bdaf865e073f280b6150b
nprimo <primo.niccolo@gmail.com>
2022-01-07 15:44:17 +0100

    add_op.c first commit

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/06 18:17:10 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/07 15:25:40 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		is_rev_sorted(t_stack *stack);
 char	*ex_op(t_stack **a, t_stack **b, char *op);
 int		pos_min(t_stack *stack);
 t_stack	*get_lis(t_stack *stack);
-void	sort3(t_stack *stack, char **comm);
+int		sort3(t_stack *stack, char **comm);
+int		add_op(t_stack **stc_a, t_stack **stc_b, char *op, char **comm);
 
 #endif
---
+++ src/add_op.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   add_op.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/07 14:46:21 by nprimo            #+#    #+#             */
+/*   Updated: 2022/01/07 15:02:42 by nprimo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	add_op(t_stack **stc_a, t_stack **stc_b, char *op, char **comm)
+{
+	char	*tmp;
+
+	tmp = *comm;
+	ex_op(stc_a, stc_b, op);
+	*comm = ft_strjoin(tmp, ex_op(stc_a, stc_b, op));
+	if (!(*comm))
+		return (0);
+	free(tmp);
+	tmp = *comm;
+	*comm = ft_strjoin(tmp, "\n");
+	if (!(*comm))
+		return (0);
+	free(tmp);
+	return (1);
+}

7fc8718c238727432af44e499acd90ffa0938905
nprimo <primo.niccolo@gmail.com>
2022-01-07 15:01:29 +0100

    updated ex_op.c
    Now checking if op is valid; returns operation if valid, NULL if not

--- src/ex_op.c
+++ src/ex_op.c
@@ -6,14 +6,34 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 17:30:29 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/07 12:09:38 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/07 14:58:48 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static int	is_valid_op(char *op)
+{
+	if (ft_strncmp(op, "sa", 2) == 0
+		|| ft_strncmp(op, "sb", 2) == 0
+		|| ft_strncmp(op, "ss", 2) == 0
+		|| ft_strncmp(op, "ra", 2) == 0
+		|| ft_strncmp(op, "rb", 2) == 0
+		|| ft_strncmp(op, "rr", 2) == 0
+		|| ft_strncmp(op, "pb", 2) == 0
+		|| ft_strncmp(op, "pa", 2) == 0
+		|| ft_strncmp(op, "rra", 3) == 0
+		|| ft_strncmp(op, "rrb", 3) == 0
+		|| ft_strncmp(op, "rrr", 3) == 0)
+		return (1);
+	else
+		return (0);
+}
+
 char	*ex_op(t_stack **a, t_stack **b, char *op)
 {
+	if (!is_valid_op(op))
+		return (NULL);
 	if (ft_strncmp(op, "sa", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)
 		ft_swap(a);
 	if (ft_strncmp(op, "sb", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)

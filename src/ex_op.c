85fcf25e7e0558911197b3e382514c655d4743e0
nprimo <primo.niccolo@gmail.com>
2022-01-07 12:13:55 +0100

    ex_op.c now working with rr, rrr and ss

--- src/ex_op.c
+++ src/ex_op.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 17:30:29 by nprimo            #+#    #+#             */
-/*   Updated: 2021/12/20 17:44:32 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/07 12:09:38 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,21 @@
 
 char	*ex_op(t_stack **a, t_stack **b, char *op)
 {
-	if (ft_strncmp(op, "sa", 2) == 0)
+	if (ft_strncmp(op, "sa", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)
 		ft_swap(a);
-	else if (ft_strncmp(op, "sb", 2) == 0)
+	if (ft_strncmp(op, "sb", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)
 		ft_swap(b);
-	else if (ft_strncmp(op, "pa", 2) == 0)
+	if (ft_strncmp(op, "pa", 2) == 0)
 		ft_push(a, b);
-	else if (ft_strncmp(op, "pb", 2) == 0)
+	if (ft_strncmp(op, "pb", 2) == 0)
 		ft_push(b, a);
-	else if (ft_strncmp(op, "ra", 2) == 0)
+	if (ft_strncmp(op, "ra", 2) == 0 || ft_strncmp(op, "rr", 2) == 0)
 		ft_rotate(a);
-	else if (ft_strncmp(op, "rb", 2) == 0)
+	if (ft_strncmp(op, "rb", 2) == 0 || ft_strncmp(op, "rr", 2) == 0)
 		ft_rotate(b);
-	else if (ft_strncmp(op, "rra", 3) == 0)
+	if (ft_strncmp(op, "rra", 3) == 0 || ft_strncmp(op, "rrr", 3) == 0)
 		ft_rev_rotate(a);
-	else if (ft_strncmp(op, "rrb", 3) == 0)
+	if (ft_strncmp(op, "rrb", 3) == 0 || ft_strncmp(op, "rrr", 3) == 0)
 		ft_rev_rotate(b);
-	else
-		op = NULL;
 	return (op);
 }

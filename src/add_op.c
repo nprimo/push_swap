1ae42ff272f5aadc9b6044dd98f88f294de74ac5
nprimo <primo.niccolo@gmail.com>
2022-01-13 17:50:33 +0100

    Fix small bug in add_op.c
    Fixed counter to avoid segfault

--- src/add_op.c
+++ src/add_op.c
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 14:46:21 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/13 17:24:03 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/13 17:49:10 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ int	add_op(t_stack **stc_a, t_stack **stc_b, char *ops, char **comm)
 {
 	char	*tmp;
 	char	**ops_list;
+	int		count;
 
 	tmp = *comm;
 	ops_list = ft_split(ops, '\n');
 	if (!ops_list)
 		return (0);
 	count = 0;
-	while (ops_list[count++])
+	while (ops_list[count])
+	{
 		ex_op(stc_a, stc_b, ops_list[count]);
+		count++;
+	}
 	*comm = ft_strjoin(tmp, ops);
 	if (!(*comm))
 		return (0);

11fe13aec4b126a2fca48653eb3add88a0261768
nprimo <primo.niccolo@gmail.com>
2022-01-13 17:24:54 +0100

    Update add_pos.c
    Now can take list of operations separated by '\n'

--- src/add_op.c
+++ src/add_op.c
@@ -6,19 +6,25 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 14:46:21 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/07 21:26:18 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/13 17:24:03 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	add_op(t_stack **stc_a, t_stack **stc_b, char *op, char **comm)
+int	add_op(t_stack **stc_a, t_stack **stc_b, char *ops, char **comm)
 {
 	char	*tmp;
+	char	**ops_list;
 
 	tmp = *comm;
-	ex_op(stc_a, stc_b, op);
-	*comm = ft_strjoin(tmp, op);
+	ops_list = ft_split(ops, '\n');
+	if (!ops_list)
+		return (0);
+	count = 0;
+	while (ops_list[count++])
+		ex_op(stc_a, stc_b, ops_list[count]);
+	*comm = ft_strjoin(tmp, ops);
 	if (!(*comm))
 		return (0);
 	free(tmp);

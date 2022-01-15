20092404ded12f68b5291d6a3c67808046215256
nprimo <primo.niccolo@gmail.com>
2022-01-15 19:29:32 +0100

    Add func init_w_stc to radix_sort.c

--- src/radix_sort.c
+++ src/radix_sort.c
@@ -6,13 +6,13 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/15 18:42:29 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/15 19:14:04 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/15 19:29:07 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static t_stack	*ins_sort(t_stack *stc)
+static t_stack	*sort_stc(t_stack *stc)
 {
 	t_stack	*sorted;
 	int		num;
@@ -28,20 +28,46 @@ static t_stack	*ins_sort(t_stack *stc)
 	return (sorted);
 }
 
-/*
+static t_stack	*init_w_stc(t_stack *stc)
+{
+	t_stack	*sorted;
+	t_stack	*w_stc;
+	int		pos;
+
+	sorted = sort_stc(stc);
+	w_stc = NULL;
+	while (stc)
+	{
+		ft_stcadd_back(&w_stc, ft_stcnew(get_stcpos(sorted, stc->num)));
+		stc = stc->next;
+	}
+	ft_stcclear(&sorted);
+	return (w_stc);
+}
+
 #include <stdio.h>
 
 int main(int ac, char **av)
 {
 	t_stack *sorted;
 	t_stack *stc;
+	t_stack	*w_stc;
+	t_stack *head;
 
 	if (ac > 1)
 	{
 		stc = init_stack(ac, av);	
-		sorted = ins_sort(stc);
+		sorted = sort_stc(stc);
+		w_stc = init_w_stc(stc);	
+	}
+	head = w_stc;
+	while (head)
+	{
+		printf("%d -- ", head->num);
+		head = head->next;
 	}
 	ft_stcclear(&sorted);
 	ft_stcclear(&stc);
+	ft_stcclear(&w_stc);
 }
-*/
+

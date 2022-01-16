781072b18a613d0c13f2bf3c505115e6dd25ad23
nprimo <primo.niccolo@gmail.com>
2022-01-16 16:10:36 +0100

    Merge ex_op functions into add_op.c

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 10:21:19 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/16 16:00:08 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/16 16:10:08 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,15 +42,18 @@ void	ft_rev_rotate(t_stack **stack);
 // sort functions
 int		is_sorted(t_stack *stack);
 int		is_rev_sorted(t_stack *stack);
-char	*ex_op(t_stack **a, t_stack **b, char *op);
-int		get_pos_min(t_stack *stack);
-t_stack	*get_lis(t_stack *stack);
-int		sort_small_stc(t_stack **stc_a, t_stack **stc_b, char **comm);
 int		add_op(t_stack **stc_a, t_stack **stc_b, char *op, char **comm);
+int		sort_small_stc(t_stack **stc_a, t_stack **stc_b, char **comm);
+int		get_rel_pos(t_stack *stc, int num);
+
+int		get_pos_min(t_stack *stack);
+
+int		radix_sort(t_stack **stc_a, t_stack **stc_b, char **comm, int count);
+
+// unused
+t_stack	*get_lis(t_stack *stack);
 char	*ins_sort(t_stack *stack);
 int		quick_sort(t_stack **stc_a, t_stack **stc_b, char **comm);
-int		get_rel_pos(t_stack *stc, int num);
 t_stack	*init_w_stc(t_stack *stc);
-int		radix_sort(t_stack **stc_a, t_stack **stc_b, char **comm, int count);
 
 #endif
--- src/add_op.c
+++ src/add_op.c
@@ -6,11 +6,54 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 14:46:21 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/13 17:57:37 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/16 16:09:02 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	is_valid_op(char *op)
+{
+	if (ft_strncmp(op, "sa", 2) == 0
+		|| ft_strncmp(op, "sb", 2) == 0
+		|| ft_strncmp(op, "ss", 2) == 0
+		|| ft_strncmp(op, "ra", 2) == 0
+		|| ft_strncmp(op, "rb", 2) == 0
+		|| ft_strncmp(op, "rr", 2) == 0
+		|| ft_strncmp(op, "pb", 2) == 0
+		|| ft_strncmp(op, "pa", 2) == 0
+		|| ft_strncmp(op, "rra", 3) == 0
+		|| ft_strncmp(op, "rrb", 3) == 0
+		|| ft_strncmp(op, "rrr", 3) == 0)
+		return (1);
+	else
+		return (0);
+}
+
+static char	*ex_op(t_stack **a, t_stack **b, char *op)
+{
+	if (!is_valid_op(op))
+		return (NULL);
+	if (ft_strncmp(op, "sa", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)
+		ft_swap(a);
+	if (ft_strncmp(op, "sb", 2) == 0 || ft_strncmp(op, "ss", 2) == 0)
+		ft_swap(b);
+	if (ft_strncmp(op, "pa", 2) == 0)
+		ft_push(a, b);
+	if (ft_strncmp(op, "pb", 2) == 0)
+		ft_push(b, a);
+	if (ft_strlen(op) == 2
+		&& (ft_strncmp(op, "ra", 2) == 0 || ft_strncmp(op, "rr", 2) == 0))
+		ft_rotate(a);
+	if (ft_strlen(op) == 2
+		&& (ft_strncmp(op, "rb", 2) == 0 || ft_strncmp(op, "rr", 2) == 0))
+		ft_rotate(b);
+	if (ft_strncmp(op, "rra", 3) == 0 || ft_strncmp(op, "rrr", 3) == 0)
+		ft_rev_rotate(a);
+	if (ft_strncmp(op, "rrb", 3) == 0 || ft_strncmp(op, "rrr", 3) == 0)
+		ft_rev_rotate(b);
+	return (op);
+}
 
 static void	free_ops_list(char **av)
 {
